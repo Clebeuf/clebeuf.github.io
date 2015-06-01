a5d3affbed5a13e5bec23ffc165c255e0df0f080
clebeuf <clebeuf@uvic.ca>
2015-06-01 16:39:33 -0700

    added function to collapse nav bar when one of the links has been selected

--- js/custom.js
+++ js/custom.js
@@ -52,12 +52,13 @@ $('#navbar-example').on('hidden.bs.collapse', function() {
     $('.nav').removeClass('nav-stacked').addClass('nav-pills');
 });
 
-
 // animated scroll from http://jsfiddle.net/9SDLw/
 $('a').click(function(){
+
+    $('#navbar-example').collapse('hide');
+
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top-10
     }, 1000);
     return false;
 });
-
