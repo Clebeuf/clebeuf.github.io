0dc3cb2129e5127cf04dfe57999cff195cdaeae2
clebeuf <clebeuf@uvic.ca>
2016-02-13 13:34:53 -0800

    basic re-design of site

--- js/custom.js
+++ js/custom.js
@@ -4,14 +4,20 @@ $( "div.skillbar" ).hover(
 			function() {$( this ).fadeTo("fast", .85);}
 		);
 
+// fade effects for the text
+$(window).scroll(function () {
 
-// exerience sliders
-jQuery(document).ready(function(){
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},6000);
-	});
+    var height = $(window).height();
+    var scrollTop = $(window).scrollTop();
+    var currentScrollTop = ($('html').scrollTop() || $('body').scrollTop());
+    var currentScrollBottom = ($('html').scrollTop() || $('body').scrollTop()) + ( $(window).height() * .3);
+
+    $('.typed-content').css({'opacity': ((height - (scrollTop * 1.5)) / height)});
+    
+    $('#project').toggleClass('visible', currentScrollBottom >= $('#project').position().top);
+    $('#contact').toggleClass('visible', currentScrollBottom >= $('#contact').position().top);
+    $('#skills').toggleClass('visible', currentScrollBottom >= $('#skills').position().top);
+
 });
 
 
@@ -20,30 +26,20 @@ $('.carousel').carousel({
   interval: 15000
 })	
 
+$('#myModal').modal('show')
 
-//typed component in the jumbotron -- https://github.com/mattboldt/typed.js/
-$(function(){
-  $(".typed-header").typed({
-    strings: [
-    "Hello, world! <br> <span class='jh'>I'm Carlene Lebeuf</span> <br> a Software Developer",
-    "Hello, world! <br> <span class='jh'>I'm Carlene Lebeuf</span> <br> a Computer Scientist! ",
-    "Hello, world! <br> <span class='jh'>I'm Carlene Lebeuf</span> <br> a Graduate Student at UVic  ^1000:) "],
-    typeSpeed: 40,
-    contentType: 'html'
-  });
-});
+// temp start typed on modal close
+$('#myModal').on('hide.bs.modal', function () {
+      $(".typed-header").typed({
+        strings: [
+        "Hello, world! <br> <span class='jh'>I'm Carlene Lebeuf</span> <br> a Software Developer",
+        "Hello, world! <br> <span class='jh'>I'm Carlene Lebeuf</span> <br> a Computer Scientist! ",
+        "Hello, world! <br> <span class='jh'>I'm Carlene Lebeuf</span> <br> a Graduate Student at UVic  ^1000:) "],
+        typeSpeed: 40,
+        contentType: 'html'
+      });
+})
 
-
-//Stack menu when collapsed & remove pills
-$('#navbar-example').on('show.bs.collapse', function() {
-    $('.nav').addClass('nav-stacked').removeClass('nav-pills');
-    console.log("hello there")
-});
-
-//Unstack menu when not collapsed & add back pills
-$('#navbar-example').on('hidden.bs.collapse', function() {
-    $('.nav').removeClass('nav-stacked').addClass('nav-pills');
-});
 
 // animated scroll from http://jsfiddle.net/9SDLw/
 $('#navbar-fixed a').click(function(){
